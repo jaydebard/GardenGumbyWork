13bf755d1cbd8dcb40218c7ec315c830ab8e12db
Jay DeBard <debard@gmail.com>
2015-06-18 10:50:58 -0400

    Installed theme

---
+++ content/themes/gardenWork/footer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package rdmgumby
+ */
+?>
+
+	</div><!-- #content .site-content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+
+
+
+		</div><!-- .site-info -->
+	</footer><!-- #colophon .site-footer -->
+
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
